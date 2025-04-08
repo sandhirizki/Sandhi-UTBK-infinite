2b8d57285197f48c9b3eee62f6bd4fce8bcc1037
Sandhi Rizki Aryadi <sandhirizki1102@gmail.com>
2025-04-08 16:40:54 +0700

    Membuat Top App Bar

--- app/src/main/java/com/sandhirizki0088/skorutbk/MainActivity.kt
+++ app/src/main/java/com/sandhirizki0088/skorutbk/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.sandhirizki0088.skorutbk
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.sandhirizki0088.skorutbk.ui.theme.SkorutbkTheme
 
@@ -18,16 +23,37 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            SkorutbkTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            MainScreen()
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen(){
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = stringResource(id = R.string.app_name))
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary
+                )
+            )
+        }
+    ){ innerPadding->
+        ScreenContent(Modifier.padding((innerPadding)))
+    }
+}
+
+@Composable
+fun ScreenContent(modifier: Modifier = Modifier){
+    Text(
+        text = "Hello",
+        modifier = modifier
+    )
 }
 
 @Composable
@@ -39,9 +65,10 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 }
 
 @Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
 @Composable
 fun GreetingPreview() {
     SkorutbkTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
